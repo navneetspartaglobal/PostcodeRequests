f7aecf911631d7c7ce1ed642d8e0ba41231ce664
Lewis <ltwelftree@spartaglobal.com>
2021-04-29 11:52:13 +0100

    basic_functionality

--- get_request/app/get_postcode_program.py
+++ get_request/app/get_postcode_program.py
@@ -1,33 +1,16 @@
 import requests
 import json
-# post_code_request = requests.get("https://api.postcodes.io/postcodes/se120nb")
-#
-# print(post_code_request.status_code)
-# print(post_code_request.headers)
-#
-# print(type(post_code_request.content))
-
-"""Notes from intro to apis and requests"""
-# json_body = json.dumps({"postcodes":[" UB1 2ST", "UB4 0PZ", "UB1 3LX"]})
-#
-# headers = {"Content-type": "application/json"}
-#
-# post_multi_req = requests.post("https://api.postcodes.io/postcodes", headers=headers, data=json_body)
-#
-#
-# print(post_multi_req.json())
-
+from pprint import pprint
 
 class PostCode:
 
     def __init__(self, postcode):
         self.postcode = postcode
-        self.request_url = f"https://api.postcodes.io/postcodes/{postcode}"
+        self.headers = {"Content-Type": "application/json"}
 
     def retrieve_postcode(self):
-        self.postcode = 
+        postcode_columns = requests.get(f"https://api.postcodes.io/postcodes/{self.postcode}")
+        return dict(postcode_columns.json())["result"]
 
-    def request_info(self):
-        pass
-
-request1 = PostCode("")
+request1 = PostCode("DT9 4LS")
+pprint(request1.retrieve_postcode()['admin_county'])
