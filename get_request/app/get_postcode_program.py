4545deb9905aca8a320e46bee7e571c9c52c27f4
Lewis <ltwelftree@spartaglobal.com>
2021-04-29 14:56:41 +0100

    added get columns

--- get_request/app/get_postcode_program.py
+++ get_request/app/get_postcode_program.py
@@ -2,7 +2,6 @@ import requests
 import json
 from pprint import pprint
 
-
 class PostCode:
     def __init__(self, post_code, columns):
         self.postcode = post_code
@@ -16,7 +15,12 @@ class PostCode:
         postcode_details_dictionary = self.retrieve_postcode_details()
         return [(key, value) for (key, value) in postcode_details_dictionary if key in self.column_list]
 
+def get_columns():
+    postcode_details = requests.get(f"https://api.postcodes.io/postcodes/HP22 6DB")
+    postcode_details_dictionary = dict(postcode_details.json())["result"].items()
+    key_list = [key for (key, value) in postcode_details_dictionary]
+    return key_list
 
-request1 = PostCode("DT9 4LS", ['postcode', 'quality', 'eastings', 'northings', 'country', 'nhs_ha', 'longitude', 'latitude', 'european_electoral_region', 'primary_care_trust', 'region', 'lsoa', 'msoa', 'incode', 'outcode'])
-pprint(request1.select_values())
 
+#request1 = PostCode("DT9 4LS", ['postcode', 'quality', 'eastings', 'northings', 'country', 'nhs_ha', 'longitude', 'latitude', 'european_electoral_region', 'primary_care_trust', 'region', 'lsoa', 'msoa', 'incode', 'outcode'])
+#pprint(request1.get_collumns())
