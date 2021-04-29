72d9a004e22981283119c420b7d6738fb3211118
Navneet <npandey@spartaglobal.com>
2021-04-29 15:02:56 +0100

    main file edit

--- get_request/main.py
+++ get_request/main.py
@@ -1,5 +1,14 @@
 from app.get_postcode_program import PostCode
+import app.get_postcode_program.get_columns()
+
+def __main__():
+    pass
+
+
+def column_output():
+    get = input("Select the column you want to see")
 
 
 
 
+PostCode("HP22 6DB", [column-list])
