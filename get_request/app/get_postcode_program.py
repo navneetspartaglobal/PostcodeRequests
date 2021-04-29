c8b241d2c33d3f2f8101dc874a414d52669f2545
Lewis <ltwelftree@spartaglobal.com>
2021-04-29 12:50:29 +0100

    update 1

--- get_request/app/get_postcode_program.py
+++ get_request/app/get_postcode_program.py
@@ -2,15 +2,29 @@ import requests
 import json
 from pprint import pprint
 
-class PostCode:
 
+class PostCode:
     def __init__(self, postcode):
         self.postcode = postcode
         self.headers = {"Content-Type": "application/json"}
 
-    def retrieve_postcode(self):
+    def retrieve_postcode(self, column_list):
         postcode_columns = requests.get(f"https://api.postcodes.io/postcodes/{self.postcode}")
-        return dict(postcode_columns.json())["result"]
+        outlist = []
+        [outlist.append(dict(postcode_columns.json())["result"][column]) for column in column_list]
+        return outlist
 
 request1 = PostCode("DT9 4LS")
-pprint(request1.retrieve_postcode()['admin_county'])
+pprint(request1.retrieve_postcode(['admin_county', 'region']))
+
+
+"""
+json_body = json.dumps({"postcodes":["HP226DB"]})
+headers = {"Content-Type":"application/json"}
+
+post_multi_req = requests.post("https://api.postcodes.io/postcodes", headers=headers, data=json_body)
+
+postcode_dictionary = dict(post_multi_req.json()["result"][0]["result"]).items()
+key_list = [k for (k, v) in postcode_dictionary]
+print(list)
+"""
