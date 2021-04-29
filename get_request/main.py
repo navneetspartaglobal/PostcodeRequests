45c5921941f6f86c5be8ff89c64656ced48baea8
Navneet <npandey@spartaglobal.com>
2021-04-29 15:57:16 +0100

    main.py file modified

--- get_request/main.py
+++ get_request/main.py
@@ -1,14 +1,19 @@
 from app.get_postcode_program import PostCode
-import app.get_postcode_program.get_columns()
+from app.get_postcode_program import get_columns
 
 def __main__():
     pass
 
 
-def column_output():
-    get = input("Select the column you want to see")
+def querypostcodes(postcode_list, column_list):
+    list_output = []
+    for postcode in postcode_list:
+        postcode_instance = PostCode(postcode, column_list)
+        list_output.append(postcode_instance.select_values())
+    return list_output
+
+
+print([print(i) for i in querypostcodes(["HP22 6DB", "UB4 0PZ"],['postcode', 'country', 'longitude', 'latitude'])])
 
 
 
-
-PostCode("HP22 6DB", [column-list])
