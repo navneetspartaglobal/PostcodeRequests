80cc82c688dfeb790c7b2d532b7314bc9464ff64
Lewis <ltwelftree@spartaglobal.com>
2021-04-29 14:12:41 +0100

    class functionality added

--- get_request/app/get_postcode_program.py
+++ get_request/app/get_postcode_program.py
@@ -4,27 +4,19 @@ from pprint import pprint
 
 
 class PostCode:
-    def __init__(self, postcode):
-        self.postcode = postcode
-        self.headers = {"Content-Type": "application/json"}
+    def __init__(self, post_code, columns):
+        self.postcode = post_code
+        self.column_list = columns
 
-    def retrieve_postcode(self, column_list):
-        postcode_columns = requests.get(f"https://api.postcodes.io/postcodes/{self.postcode}")
-        outlist = []
-        [outlist.append(dict(postcode_columns.json())["result"][column]) for column in column_list]
-        return outlist
+    def retrieve_postcode_details(self):
+        postcode_details = requests.get(f"https://api.postcodes.io/postcodes/{self.postcode}")
+        return dict(postcode_details.json())["result"].items()
 
-request1 = PostCode("DT9 4LS")
-pprint(request1.retrieve_postcode(['admin_county', 'region']))
+    def select_values(self):
+        postcode_details_dictionary = self.retrieve_postcode_details()
+        return [(key, value) for (key, value) in postcode_details_dictionary if key in self.column_list]
 
 
-"""
-json_body = json.dumps({"postcodes":["HP226DB"]})
-headers = {"Content-Type":"application/json"}
+request1 = PostCode("DT9 4LS", ['postcode', 'quality', 'eastings', 'northings', 'country', 'nhs_ha', 'longitude', 'latitude', 'european_electoral_region', 'primary_care_trust', 'region', 'lsoa', 'msoa', 'incode', 'outcode'])
+pprint(request1.select_values())
 
-post_multi_req = requests.post("https://api.postcodes.io/postcodes", headers=headers, data=json_body)
-
-postcode_dictionary = dict(post_multi_req.json()["result"][0]["result"]).items()
-key_list = [k for (k, v) in postcode_dictionary]
-print(list)
-"""
