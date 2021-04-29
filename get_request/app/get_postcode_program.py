a1e31fed06212c97414bd66fbecd223ce0defcd1
Navneet <npandey@spartaglobal.com>
2021-04-29 13:40:15 +0100

    update 2

--- get_request/app/get_postcode_program.py
+++ get_request/app/get_postcode_program.py
@@ -2,6 +2,7 @@ import requests
 import json
 from pprint import pprint
 
+
 class PostCode:
 
     def __init__(self, postcode):
@@ -11,6 +12,7 @@ class PostCode:
     def retrieve_postcode(self):
         postcode_columns = requests.get(f"https://api.postcodes.io/postcodes/{self.postcode}")
         return dict(postcode_columns.json())["result"]
+
 
 request1 = PostCode("DT9 4LS")
 pprint(request1.retrieve_postcode()['admin_county'])
